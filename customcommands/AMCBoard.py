9cd07ed969ac3d8d72090f25fdb1b4dc9a9344b9
unknown <williamcarter808@gmail.com>
2023-06-16 16:42:08 +1000

    Added AMC board command

--- customcommands/AMCBoard.py
+++ customcommands/AMCBoard.py
@@ -6,15 +6,15 @@ from customcommands.customvalidations import *
 
 import neatTables
 
-class AverageRankBoardCommand(cobble.command.Command):
+class AMCBoardCommand(cobble.command.Command):
     def __init__(self, bot: cobble.bot.Bot):
         """
         Parameters:
             bot - The bot object the command will belong to
         """
-        super().__init__(bot, "Average Rank Leaderboard", "arboard", "Show the leaderboard for average ranks", cobble.permissions.EVERYONE)
+        super().__init__(bot, "All Main Categories Leaderboard", ["amcboard", "amcb"], "Show the leaderboard for the sum of a person's PBs", cobble.permissions.EVERYONE)
         self.addArgument(cobble.command.Argument("start", "What place to start from", cobble.validations.IsInteger(), True))
-        self.addArgument(cobble.command.Argument("useCache", "Whether to use the stored values or recalculate them", cobble.validations.IsBool(), True))
+        
         
 
     async def execute(self, messageObject: discord.message, argumentValues: dict, attachedFiles: dict) -> str:
@@ -28,19 +28,15 @@ class AverageRankBoardCommand(cobble.command.Command):
 
         argumentValues["start"] = int(argumentValues["start"])
 
-        useCache = True
-        if "useCache" in argumentValues:
-            if argumentValues["useCache"] == "false":
-                useCache = False
-                
-        averageRanks = dbm.getAverageRankLeaderboard(useCache)[argumentValues["start"]-1:argumentValues["start"]+19]
-        
-        tableData = [["Place", "Runner", "Average Rank"]]
-        for index, entry in enumerate(averageRanks):
+        amcBoard = dbm.generateAMCBoard()[argumentValues["start"]-1:argumentValues["start"]+19]
+
+        tableData = [["Place", "Runner", "AMC Estimate"]]
+        for index, entry in enumerate(amcBoard):
             tableData.append([
                 str(index+argumentValues["start"])+".", 
-                entry[0], 
-                str(entry[1])])
-
+                entry["name"], 
+                durations.formatted(entry["time"])
+                ])
+            
         table = "```"+neatTables.generateTable(tableData, padding=2)+"```"
         return table
